e279a6d73c425098641f2ee43eeb3c73fef06865
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2020-09-24 15:08:04 +0700

    Update OperatorEquality.java

--- Modul1Basic/Operator/src/com/dicoding/javafundamental/operator/OperatorEquality.java
+++ Modul1Basic/Operator/src/com/dicoding/javafundamental/operator/OperatorEquality.java
@@ -4,42 +4,37 @@ public class OperatorEquality {
 
     public static void main(String[] args) {
 
-        int value1 = 5;
-        int value2 = 4;
-        boolean hasil;
-        hasil = value1 == value2;
+        int value = 5;
+        int anotherValue = 4;
+        boolean result;
+        result = value == anotherValue;
 
-        System.out.println("Hasil 'value1 == value2' adalah " + hasil);
+        System.out.println("Hasil 'value == anotherValue' adalah " + result);
         System.out.println();
+        
         System.out.println("Tidak sama dengan..");
-
-        hasil = value1 != value2;
-
-        System.out.println("Hasil 'value1 != value2' adalah " + hasil);
+        result = value != anotherValue;
+        System.out.println("Hasil 'value != anotherValue' adalah " + result);
         System.out.println();
+        
         System.out.println("Lebih besar dari..");
-
-        hasil = value1 > value2;
-
-        System.out.println("Hasil 'value1 > value2' adalah " + hasil);
+        result = value > anotherValue;
+        System.out.println("Hasil 'value > anotherValue' adalah " + result);
         System.out.println();
+        
         System.out.println("Sama atau lebih besar dari..");
-
-        hasil = value1 >= value2;
-
-        System.out.println("Hasil 'value1 >= value2' adalah " + hasil);
+        result = value >= anotherValue;
+        System.out.println("Hasil 'value >= anotherValue' adalah " + result);
         System.out.println();
+        
         System.out.println("Kurang dari..");
-
-        hasil = value1 < value2;
-
-        System.out.println("Hasil 'value1 < value2' adalah " + hasil);
+        result = value < anotherValue;
+        System.out.println("Hasil 'value < anotherValue' adalah " + result);
         System.out.println();
+        
         System.out.println("Sama atau kurang dari dengan..");
-
-        hasil = value1 <= value2;
-
-        System.out.println("Hasil 'value1 <= value2' adalah " + hasil);
+        result = value <= anotherValue;
+        System.out.println("Hasil 'value <= anotherValue' adalah " + result);
         System.out.println();
 
     }
