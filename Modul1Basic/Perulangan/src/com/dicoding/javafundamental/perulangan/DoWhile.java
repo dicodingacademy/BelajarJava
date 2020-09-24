738366a3f515060fbfc4c9590c892b5eb7d9ff62
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2020-09-24 15:14:27 +0700

    Update DoWhile.java

--- Modul1Basic/Perulangan/src/com/dicoding/javafundamental/perulangan/DoWhile.java
+++ Modul1Basic/Perulangan/src/com/dicoding/javafundamental/perulangan/DoWhile.java
@@ -2,10 +2,10 @@ package com.dicoding.javafundamental.perulangan;
 
 public class DoWhile {
     public static void main(String[] args) {
-        int nilai = 1;
+        int value = 1;
         do {
-            System.out.println("Angka : " + nilai);
-            nilai++;
-        } while (nilai <= 10);
+            System.out.println("Angka : " + value);
+            value++;
+        } while (value <= 10);
     }
 }
