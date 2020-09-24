aa51fb3c23428b8360f584f74ee4b890fc3e0610
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2020-09-24 15:14:19 +0700

    Update While.java

--- Modul1Basic/Perulangan/src/com/dicoding/javafundamental/perulangan/While.java
+++ Modul1Basic/Perulangan/src/com/dicoding/javafundamental/perulangan/While.java
@@ -2,10 +2,10 @@ package com.dicoding.javafundamental.perulangan;
 
 public class While {
     public static void main(String[] args) {
-        int nilai = 1;
-        while (nilai <= 10) {
-            System.out.print("Angka : " + nilai);
-            nilai++;
+        int value = 1;
+        while (value <= 10) {
+            System.out.print("Angka : " + value);
+            value++;
             System.out.print("\n");
         }
     }
