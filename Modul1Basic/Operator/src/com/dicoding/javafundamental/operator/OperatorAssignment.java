10b339ba2cc33a9eb4421c9f9ef8e0c64ade86e1
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2020-09-24 15:08:39 +0700

    Update OperatorAssignment.java

--- Modul1Basic/Operator/src/com/dicoding/javafundamental/operator/OperatorAssignment.java
+++ Modul1Basic/Operator/src/com/dicoding/javafundamental/operator/OperatorAssignment.java
@@ -4,10 +4,10 @@ public class OperatorAssignment {
 
     public static void main(String[] args) {
 
-        int mData1 = 2;
-        int mData2 = 19;
-        System.out.println("Data pada mData1 adalah ->> " + mData1);
-        System.out.println("Data pada mData2 adalah ->> " + mData2);
+        int value = 2;
+        int anotherValue = 19;
+        System.out.println("Data pada value adalah ->> " + value);
+        System.out.println("Data pada anotherValue adalah ->> " + anotherValue);
 
     }
 }
