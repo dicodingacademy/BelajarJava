10d91a02f1b4457323f4a1ef7db5936a434f5c7c
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2020-09-24 11:37:58 +0700

    Update OperatorConditional.java

--- Modul1Basic/Operator/src/com/dicoding/javafundamental/operator/OperatorConditional.java
+++ Modul1Basic/Operator/src/com/dicoding/javafundamental/operator/OperatorConditional.java
@@ -3,23 +3,23 @@ package com.dicoding.javafundamental.operator;
 public class OperatorConditional {
 
     public static void main(String[] args) {
-        int value1 = 4;
-        int value2 = 5;
+        int value = 4;
+        int anotherValue = 5;
 
         System.out.println("Conditional AND");
-        boolean hasil = value1 == 3 && value2 == 5;
-        boolean hasil2 = value1 != 3 && value2 == 5;
+        boolean result = value == 3 && anotherValue == 5;
+        boolean anotherResult = value != 3 && anotherValue == 5;
 
-        System.out.println("Hasil Operator AND pada syarat value1 == 3 dan value2 == 5 adalah " + hasil);
-        System.out.println("Hasil Operator AND pada syarat value1 != 3 dan value2 == 5 adalah " + hasil2);
+        System.out.println("Hasil Operator AND pada syarat value == 3 dan anotherValue == 5 adalah " + result);
+        System.out.println("Hasil Operator AND pada syarat value != 3 dan anotherValue == 5 adalah " + anotherResult);
         System.out.println();
         System.out.println("Conditional OR");
 
-        hasil = value1 == 3 || value2 == 5;
-        hasil2 = value1 != 3 || value2 == 5;
+        result = value == 3 || anotherValue == 5;
+        anotherResult = value != 3 || anotherValue == 5;
 
-        System.out.println("Hasil Operator AND pada syarat value1 == 3 dan value2 == 5 adalah " + hasil);
-        System.out.println("Hasil Operator AND pada syarat value1 != 3 dan value2 == 5 adalah " + hasil2);
+        System.out.println("Hasil Operator OR pada syarat value == 3 dan anotherValue == 5 adalah " + result);
+        System.out.println("Hasil Operator OR pada syarat value != 3 dan anotherValue == 5 adalah " + anotherResult);
         System.out.println();
     }
 }
